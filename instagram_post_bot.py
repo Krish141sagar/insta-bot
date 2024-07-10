5634a5faba8cb4f524fcd03b8aceddbabe530bee
Krish141sagar <41360038+Krish141sagar@users.noreply.github.com>
2024-07-10 19:29:04 +0530

    Update instagram_post_bot.py
    Fix

--- instagram_post_bot.py
+++ instagram_post_bot.py
@@ -1,4 +1,4 @@
-import os
+8import os
 import requests
 import logging
 from dotenv import load_dotenv
@@ -8,6 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 
